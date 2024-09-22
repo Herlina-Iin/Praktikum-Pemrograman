27ea0881bc2779732990582f7ac3129ae36a9e04
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-09-22 11:38:08 +0700

    Update sum.cpp

--- Pertemuan 4/Soal 1/sum.cpp
+++ Pertemuan 4/Soal 1/sum.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int sum(int n) {
+    int jumlah = 0;
+    for (int i = 1; i <= n; ++i) {
+        jumlah += i;
+    }
+    return jumlah;
+}
+
+int main() {
+    int n;
+
+    cout << "n: ";
+    cin >> n;
+
+    cout << "Jumlah dari 1 hingga " << n << " : " << sum(n) << endl;
+
+    return 0;
+}
