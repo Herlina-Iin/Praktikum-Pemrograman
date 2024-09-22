469f9f303f80479a6af57b6ce6bf62642727165c
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-09-22 15:54:59 +0700

    Update multiplication.cpp

--- Pertemuan 4/Soal 2/multiplication.cpp
+++ Pertemuan 4/Soal 2/multiplication.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+
+    cout << "Masukkan angka: ";
+    cin >> n;
+
+    cout << "Tabel Perkalian untuk " << n << ":\n";
+
+    for(int i = 1; i <= 10; ++i) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
