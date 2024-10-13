b0304a9fd31211c170ac10671dd4243738b35798
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-10-13 17:12:50 +0700

    Update reverse.cpp

--- Pertemuan 5/Problem 2/reverse.cpp
+++ Pertemuan 5/Problem 2/reverse.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+vector<int>reverseArraykeVector(int arr[], int size){
+    vector<int>reversed;
+
+    for(int i=size-1;i>=0;i--){
+        reversed.push_back(arr[i]);
+    }
+    return reversed;
+}
+
+int main(){
+    int n;
+    cout<<"Banyak angka : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Masukkan "<<n<<" angka : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>reversed=reverseArraykeVector(arr,n);
+    cout<<"Vektor terbalik : ";
+    for(const int& num:reversed){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
