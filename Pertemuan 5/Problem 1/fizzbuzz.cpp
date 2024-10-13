bf984652390bd270f5465a6f70cf22a2e4ba7f35
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-10-13 16:24:29 +0700

    Update fizzbuzz.cpp

--- Pertemuan 5/Problem 1/fizzbuzz.cpp
+++ Pertemuan 5/Problem 1/fizzbuzz.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
 
+int main() {
+    vector<string>hasil;
+    
+    for(int i=1;i<=100;i++){
+        if(i%3==0&&i%5==0){
+            hasil.push_back("FizzBuzz");
+        } else if(i%3==0){
+            hasil.push_back("Fizz");
+        } else if(i%5==0){
+            hasil.push_back("Buzz");
+        } else{
+            hasil.push_back(to_string(i));
+        }
+    }
+    for(const string& str:hasil){
+        cout<<str<<endl;
+    }
+        
+    return 0;
+}
