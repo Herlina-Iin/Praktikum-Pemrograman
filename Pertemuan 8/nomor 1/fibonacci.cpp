d155484be7a4879521c416d39603a17788aebcd9
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-11-05 14:14:28 +0700

    Create fibonacci.cpp

---
+++ Pertemuan 8/nomor 1/fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci (int n); {
+    if (n<=1) return n;
+    else
+        return fibonacci(n-1)+fibonacci(n-2);
+}
+int main(){
+    int n;
+    cout<<"Masukkan n : ";
+    cin>>n;
+
+    int hasil=fibonacci(n);
+    cout<<"Bilangan fibonaccci ke-"<<n<<" adalah : "<<hasil<<endl;
+
+    return 0;
+}
