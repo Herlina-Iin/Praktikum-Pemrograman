e766a901b974569fddfe3f233674c3246a60063e
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-11-26 13:39:43 +0700

    Update code.cpp

--- Responsi/code 1/code.cpp
+++ Responsi/code 1/code.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    double r;
+    const double pi=3.14;
+    cout<<"Masukkan Jari-jari: ";
+    cin>>r;
+    
+    double luas=pi*r*r;
+    cout<<"Luas Lingkaran: "<<luas<<endl;
+    
+    return 0;
+}
