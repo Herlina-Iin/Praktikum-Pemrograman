69fd780012ae1c34417c4956a0956fe45b89472b
Herlina-Iin <herlinaiinnursoleha2006@mail.ugm.ac.id>
2024-11-26 14:36:25 +0700

    Update code.cpp

--- Responsi/code 2/code.cpp
+++ Responsi/code 2/code.cpp
@@ -5,36 +5,36 @@ using namespace std;
 int main() {
     int n1;
     int n2;
-    cout << "Masukkan jumlah elemen pertama: ";
-    cin >> n1;
+    cout<<"Masukkan jumlah elemen pertama: ";
+    cin>>n1;
 
     int daftar1[n1];
-    cout << "Masukkan elemen pertama (terurut): ";
-    for (int i = 0; i < n1; i++) {
-        cin >> daftar1[i];
+    cout<<"Masukkan elemen pertama (terurut): ";
+    for (int i=0; i<n1; i++) {
+        cin>>daftar1[i];
     }
-    cout << "Masukkan jumlah elemen kedua: ";
-    cin >> n2;
+    cout<<"Masukkan jumlah elemen kedua: ";
+    cin>>n2;
 
     int daftar2[n2];
-    cout << "Masukkan elemen kedua (terurut): ";
-    for (int i = 0; i < n2; i++) {
-        cin >> daftar2[i];
+    cout<<"Masukkan elemen kedua (terurut): ";
+    for (int i=0; i<n2; i++) {
+        cin>>daftar2[i];
     }
     int gabungan[n1 + n2];
-    for (int i = 0; i < n1; i++) {
-        gabungan[i] = daftar1[i];
+    for (int i=0; i<n1; i++) {
+        gabungan[i]=daftar1[i];
     }    
-    for (int i = 0; i < n2; i++) {
-        gabungan[n1 + i] = daftar2[i];
+    for (int i=0; i<n2; i++) {
+        gabungan[n1+i]=daftar2[i];
     }
-    sort(gabungan, gabungan + n1 + n2);
+    sort(gabungan, gabungan+n1+n2);
 
-    cout << "Daftar gabungan terurut: ";
-    for (int i = 0; i < n1 + n2; i++) {
-        cout << gabungan[i] << " ";
+    cout<<"Daftar gabungan terurut: ";
+    for (int i=0; i<n1+n2; i++) {
+        cout<<gabungan[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
 
     return 0;
 }
